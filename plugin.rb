336e628d85939689ffb33ee7d0df1c3ed1a6db4e
neha01kbizsoft <151748120+neha01kbizsoft@users.noreply.github.com>
2024-05-03 12:02:34 +0530

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+
+# frozen_string_literal: true
+# name: rating-plugin
+# about: I want to develop a Discourse plugin that allows users to rate one another after a transaction. 
+# version: 0.0.1
+# authors: kbiz Plugin Developer
+# url: https://github.com/neha01kbizsoft/rating-plugin.git
+
+
+
+
